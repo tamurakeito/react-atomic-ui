bf17bb455c6058b2afb56c5fc790f7961578ef67
tamurakeito <kit_tamtam@icloud.com>
2024-11-19 17:27:05 +0900

    yarn commit: button ui 実装

--- atoms/button/index.stories.tsx
+++ atoms/button/index.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Button from ".";
+import Button, { buttonSizes, buttonVariants } from ".";
 
 const meta = {
   title: "Atom/Button",
@@ -13,9 +13,138 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Button_: Story = {
+export const Button_primary_sm: Story = {
   args: {
     children: "サインイン",
-    onClick: () => {},
+    variant: buttonVariants.primary,
+    size: buttonSizes.sm,
+  },
+};
+
+export const Button_primary_sm_disabled: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.primary,
+    size: buttonSizes.sm,
+    disabled: true,
+  },
+};
+
+export const Button_primary_md: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.primary,
+    size: buttonSizes.md,
+  },
+};
+
+export const Button_primary_md_disabled: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.primary,
+    size: buttonSizes.md,
+    disabled: true,
+  },
+};
+
+export const Button_primary_xs: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.primary,
+    size: buttonSizes.xs,
+  },
+};
+
+export const Button_primary_xs_disabled: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.primary,
+    size: buttonSizes.xs,
+    disabled: true,
+  },
+};
+
+export const Button_white_sm: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.white,
+    size: buttonSizes.sm,
+  },
+};
+
+export const Button_white_sm_disabled: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.white,
+    size: buttonSizes.sm,
+    disabled: true,
+  },
+};
+
+export const Button_light_sm: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.light,
+    size: buttonSizes.sm,
+  },
+};
+
+export const Button_light_sm_disabled: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.light,
+    size: buttonSizes.sm,
+    disabled: true,
+  },
+};
+
+export const Button_danger_sm: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.danger,
+    size: buttonSizes.sm,
+  },
+};
+
+export const Button_danger_sm_disabled: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.danger,
+    size: buttonSizes.sm,
+    disabled: true,
+  },
+};
+
+export const Button_outline_sm: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.outline,
+    size: buttonSizes.sm,
+  },
+};
+
+export const Button_outline_sm_disabled: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.outline,
+    size: buttonSizes.sm,
+    disabled: true,
+  },
+};
+
+export const Button_primaryLight_sm: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.primaryLight,
+    size: buttonSizes.sm,
+  },
+};
+
+export const Button_primaryLight_sm_disabled: Story = {
+  args: {
+    children: "サインイン",
+    variant: buttonVariants.primaryLight,
+    size: buttonSizes.sm,
+    disabled: true,
   },
 };
--- atoms/button/index.tsx
+++ atoms/button/index.tsx
@@ -1,23 +1,65 @@
-import { ReactNode } from "react";
+import { forwardRef, ReactNode } from "react";
 import classes from "./styles.module.scss";
 import classNames from "classnames";
 
+export type ButtonProps = {
+  children?: ReactNode;
+  variant?: ButtonVariant;
+  className?: string;
+  size?: ButtonSize;
+  disabled?: boolean;
+  onClick?: () => void;
+};
+
 const Button = ({
   children,
+  variant = buttonVariants.primary,
+  size = buttonSizes.sm,
   className,
   onClick,
+  disabled = false,
   ...props
-}: {
-  children: ReactNode;
-  className?: string;
-  onClick: () => void;
-}) => {
-  const clazz = classNames([classes.button, className]);
+}: ButtonProps) => {
+  const clazz = classNames(
+    classes.button,
+    classes[`button_${variant}`],
+    classes[`button_${size}`],
+    {
+      [classes[`button_${variant}_disabled`]]: disabled,
+    },
+    className
+  );
+  const handleClick = () => {
+    !disabled && onClick && onClick();
+  };
   return (
-    <div className={clazz} onClick={onClick} {...props}>
+    <button
+      className={clazz}
+      onClick={handleClick}
+      disabled={disabled}
+      {...props}
+    >
       {children}
-    </div>
+    </button>
   );
 };
 
 export default Button;
+
+export const buttonVariants = {
+  primary: "primary",
+  white: "white",
+  light: "light",
+  danger: "danger",
+  outline: "outline",
+  primaryLight: "primaryLight",
+} as const;
+export type ButtonVariant =
+  (typeof buttonVariants)[keyof typeof buttonVariants];
+
+export const buttonSizes = {
+  sm: "sm",
+  md: "md",
+  xs: "xs",
+} as const;
+export type ButtonSize = (typeof buttonSizes)[keyof typeof buttonSizes];
